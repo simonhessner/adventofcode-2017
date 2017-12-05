31a8cc8779de0e31fc45d52ffbb689fe33f7495b
Simon Hessner <uldci@student.kit.edu>
2017-12-05 06:18:46 +0100

    Add solution for day 5

---
+++ day5/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# http://adventofcode.com/2017/day/5
+
+import sys
+import numpy
+import math
+
+def solve(part = 1):
+	cur_pos = 0
+	steps = 0
+
+	with open("input") as inputfile:
+		instructions = [int(x) for x in inputfile.read().splitlines()]
+
+		while True:
+			if cur_pos < 0 or cur_pos >= len(instructions):
+				break
+			steps += 1
+			instruction = instructions[cur_pos]
+			instructions[cur_pos] += 1 if (instructions[cur_pos] < 3 or part == 1) else -1		
+			cur_pos += instruction	
+
+		return steps
+
+print("part 1", solve(1))
+print("part 2", solve(2))
