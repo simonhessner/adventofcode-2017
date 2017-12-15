7d57b46211082c85413de5d56f0f16af27a18ce5
Simon Hessner <uldci@student.kit.edu>
2017-12-15 06:44:30 +0100

    Merge the two generators in one general generator to save LOC

--- day15/solution.py
+++ day15/solution.py
@@ -2,29 +2,20 @@
 
 # http://adventofcode.com/2017/day/15
 
-def genA(mulof=1):
-	val = 873
+start_a  = 873
+start_b  = 583
+factor_a = 16807
+factor_b = 48271
+
+def genfunc(val,factor,mulof=1):	
 	while True:
-		val = (val*16807) % 2147483647
+		val = (val*factor) % 2147483647
 		if val % mulof == 0:
 			yield val
-
-def genB(mulof=1):
-	val = 583
-	while True:
-		val = (val*48271) % 2147483647
-		if val % mulof == 0:
-			yield val
-
-def generator_func(start, factor, divisor, mulof):
-	val = (start*factor) % divisor
-	while val % mulof != 0:
-		val = (val*factor) % divisor
-	return val
 
 n = 0
-gena = genA()
-genb = genB()
+gena = genfunc(start_a, factor_a)
+genb = genfunc(start_b, factor_b)
 
 for i in range(40000000):
 	# & 0xFFFF extracts the lower 4*4 = 16 bits without needing
@@ -34,11 +25,9 @@ for i in range(40000000):
 
 print("part 1", n)
 
-# ---------------------------------------------
-
 n = 0
-gena = genA(mulof=4)
-genb = genB(mulof=8)
+gena = genfunc(start_a, factor_a, mulof=4)
+genb = genfunc(start_b, factor_b, mulof=8)
 
 for i in range(5000000):
 	if (next(gena) & 0xFFFF == next(genb) & 0xFFFF):
