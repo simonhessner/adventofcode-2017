b69dfff9fcbf059cb652ab1a0340923d3e0c9c1a
Simon Hessner <uldci@student.kit.edu>
2017-12-15 06:52:31 +0100

    Save more LOC in the solution for day 15

--- day15/solution.py
+++ day15/solution.py
@@ -11,26 +11,16 @@ def genfunc(val,factor,mulof=1):
 	while True:
 		val = (val*factor) % 2147483647
 		if val % mulof == 0:
-			yield val
+			# & 0xFFFF extracts the lower 4*4 = 16 bits without needing
+			# to convert the number to a string and then slicing it
+			yield val & 0xFFFF
 
-n = 0
 gena = genfunc(start_a, factor_a)
 genb = genfunc(start_b, factor_b)
-
-for i in range(40000000):
-	# & 0xFFFF extracts the lower 4*4 = 16 bits without needing
-	# to convert the number to a string and then slicing it
-	if (next(gena) & 0xFFFF == next(genb) & 0xFFFF):
-		n += 1
-
+n = sum(next(gena) == next(genb) for _ in range(40000000))
 print("part 1", n)
 
-n = 0
 gena = genfunc(start_a, factor_a, mulof=4)
 genb = genfunc(start_b, factor_b, mulof=8)
-
-for i in range(5000000):
-	if (next(gena) & 0xFFFF == next(genb) & 0xFFFF):
-		n += 1
-
+n = sum(next(gena) == next(genb) for _ in range(5000000))
 print("part 2", n)
