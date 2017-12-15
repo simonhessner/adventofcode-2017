009d1274e268aaef396dc8b1df3df643965c542a
Simon Hessner <uldci@student.kit.edu>
2017-12-15 06:39:52 +0100

    Rewrite day 15 to use bit masking instead of string slicing and generators to produce the results

--- day15/solution.py
+++ day15/solution.py
@@ -2,12 +2,19 @@
 
 # http://adventofcode.com/2017/day/15
 
-start_a = 873
-start_b = 583
+def genA(mulof=1):
+	val = 873
+	while True:
+		val = (val*16807) % 2147483647
+		if val % mulof == 0:
+			yield val
 
-factor_a = 16807
-factor_b = 48271
-divisor  = 2147483647
+def genB(mulof=1):
+	val = 583
+	while True:
+		val = (val*48271) % 2147483647
+		if val % mulof == 0:
+			yield val
 
 def generator_func(start, factor, divisor, mulof):
 	val = (start*factor) % divisor
@@ -15,30 +22,26 @@ def generator_func(start, factor, divisor, mulof):
 		val = (val*factor) % divisor
 	return val
 
-a = start_a
-b = start_b
 n = 0
+gena = genA()
+genb = genB()
 
 for i in range(40000000):
-	a = generator_func(a, factor_a, divisor, 1)
-	b = generator_func(b, factor_b, divisor, 1)
-
-	if ("{0:016b}".format(a)[-16:] == "{0:016b}".format(b)[-16:]):
+	# & 0xFFFF extracts the lower 4*4 = 16 bits without needing
+	# to convert the number to a string and then slicing it
+	if (next(gena) & 0xFFFF == next(genb) & 0xFFFF):
 		n += 1
 
 print("part 1", n)
 
 # ---------------------------------------------
 
-a = start_a
-b = start_b
 n = 0
+gena = genA(mulof=4)
+genb = genB(mulof=8)
 
 for i in range(5000000):
-	a = generator_func(a, factor_a, divisor, 4)
-	b = generator_func(b, factor_b, divisor, 8)
-
-	if ("{0:016b}".format(a)[-16:] == "{0:016b}".format(b)[-16:]):
+	if (next(gena) & 0xFFFF == next(genb) & 0xFFFF):
 		n += 1
 
 print("part 2", n)
