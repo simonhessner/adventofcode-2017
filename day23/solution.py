10bbde3b6d0c01099dd14ec90cb860625e3ddaf4
Simon Hessner <uldci@student.kit.edu>
2017-12-23 07:30:37 +0100

    Remove code redundancy

--- day23/solution.py
+++ day23/solution.py
@@ -6,8 +6,6 @@ import collections
 
 registers = collections.defaultdict(int)
 
-#registers["a"] = 1
-
 def getreg(reg):
 	try:
 		return int(reg)
@@ -21,31 +19,28 @@ with open("input") as inputfile:
 	n = 0
 
 	while i >= 0 and i < len(commands):
-		cmd = commands[i]
-		parts = cmd.split(" ")
+		command = commands[i]
+		cmd, dst, src = command.split(" ")
 
-		if parts[0] == "set":
-			registers[parts[1]] = getreg(parts[2])
-			i += 1
-		elif parts[0] == "sub":
-			registers[parts[1]] -= getreg(parts[2])
-			i += 1
-		elif parts[0] == "mul":
-			registers[parts[1]] *= getreg(parts[2])
-			i += 1
+		i += 1
+
+		if cmd == "set":
+			registers[dst]  = getreg(src)
+		elif cmd == "sub":
+			registers[dst] -= getreg(src)
+		elif cmd == "mul":
+			registers[dst] *= getreg(src)
 			n += 1
-		elif parts[0] == "jnz":
-			if getreg(parts[1]) != 0:
-				i += getreg(parts[2])
-			else:
-				i += 1
+		elif cmd == "jnz":
+			if getreg(dst) != 0:
+				i += (getreg(src) - 1) # -1 because we add 1 in all cases to save LOC
 		else:
 			print("error")
 	
 	print("part 1", n)
 
-##
-## Part 2: Thanks to reddit: https://www.reddit.com/r/adventofcode/comments/7lms6p/2017_day_23_solutions/
+# Part 2 was hard: 
+# Thanks to reddit: https://www.reddit.com/r/adventofcode/comments/7lms6p/2017_day_23_solutions/
 
 n = 0
 for i in range(106500, 123500 + 1, 17):
